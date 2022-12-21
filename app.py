231aef5f252bb084271975a6d61a193cc12c4ab3
elii03611 <elii03611@gmail.com>
2022-12-21 19:20:51 +0200

    flask-cors

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-api = Flask(__name__)
-CORS(api)
-@api.route('/')
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
 def hello():
     return 'Hello, World!'
 
 myData= [{"name":"eli","age":26},{"name":"avi","age":20}]
-@api.route('/students')
+@app.route('/students')
 def students():
     return myData
  
  
 if __name__ == '__main__':
-    api.run(debug=True)
+    app.run(debug=True)
